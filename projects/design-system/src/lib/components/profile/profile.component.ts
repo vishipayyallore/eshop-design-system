084896843dfeae5d1d3d5a822b92e4a595b4e222
Robert Collins <roberto.tomas.cuentas@gmail.com>
2022-09-10 08:09:30 -0400

    wip (#3)

--- projects/design-system/src/lib/components/profile/profile.component.ts
+++ projects/design-system/src/lib/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy } from '@angular/core'
 
 import { ProfileService } from './profile.service'
 import { DEFAULT_PROFILE_HEADER } from './copy'
@@ -7,16 +8,25 @@ import { DEFAULT_PROFILE_HEADER } from './copy'
 @Component({
   selector: 'eshop-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent {
   @Input() isShowingIconView?: boolean
-  @Input() isShowingPageView?: boolean
-  profileIcon$ = this.profileService.profileIcon$
+
+  get isShowingPageView() {
+    return !this.isShowingIconView;
+  }
+
+  get profileIcon$ () {
+    return this.profileService.profileIcon$
+  }
 
   copy = { header: DEFAULT_PROFILE_HEADER }
 
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService) { 
+    (window as any).component = this
+  }
 
   onClickProfileIcon() {
     this.profileService.navigateToProfile.next(null)
