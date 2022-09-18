c33a3b34b193002a48f41f5b409fbc565a25436c
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2022-09-18 09:36:47 -0400

    revised exports

--- projects/design-system/src/lib/design-system.module.ts
+++ projects/design-system/src/lib/design-system.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from "@angular/core"
 
 import { HeaderModule } from "./components/header/header.module"
+export { HeaderModule } from "./components/header/header.module"
 import { ProfileModule } from "./components/profile/profile.module"
+export { ProfileModule } from "./components/profile/profile.module"
 import { MainMenuModule } from "./components/main-menu/main-menu.module"
+export { MainMenuModule } from "./components/main-menu/main-menu.module"
 import { PageDetailModule } from "./components/page-detail/page-detail.module"
+export { PageDetailModule } from "./components/page-detail/page-detail.module"
 import { BrandingModule } from "./components/branding/branding.module"
+export { BrandingModule } from "./components/branding/branding.module"
 import { AccordionModule } from "./components/accordion/accordion.module"
+export { AccordionModule } from "./components/accordion/accordion.module"
 import { FooterModule } from "./components/footer/footer.module"
+export { FooterModule } from "./components/footer/footer.module"
 
 @NgModule({
   imports: [
+    HeaderModule, ProfileModule, MainMenuModule,
+    PageDetailModule, BrandingModule, AccordionModule,
+    FooterModule
+  ],
+  exports: [
     HeaderModule, ProfileModule, MainMenuModule,
     PageDetailModule, BrandingModule, AccordionModule,
     FooterModule
